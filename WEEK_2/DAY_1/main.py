ac1242af70a3e1fbd9838cb4b6f9c78251ce66ed
Ola Senatov <ola@senatov.pro>
2023-07-16 13:38:18 +0300

    start of week 2

---
+++ WEEK_2/DAY_1/main.py
@@ -0,0 +1,33 @@
+# my_current_age = 56
+# age_in_future = my_current_age + 123879
+# print(f"my age in the future will be {age_in_future}")
+
+
+# number_of_miles = input("tell me how many miles \n")
+# kilometer = float(number_of_miles) * 1.60934
+# kilometer = round(number_of_miles * 1.60934,2)
+# print(number_of_miles, "miles are in km", kilometer)
+
+# name = 'John Doe'
+# if len(name)>=20:
+#     print(f'Name "{name}" is more than 20 chars long')
+#     length_description = 'long'
+# elif len(name)>=15:
+#     print(f'Name "{name}" is more than 15 chars long')
+#     length_description = 'semi long'
+# elif len(name)>=10:
+#     print(f'Name "{name}" is more than 10 chars long')
+#     length_description = 'semi long'
+# elif 10>=len(name)>=8:
+#     print(f'Name "{name}" is 8, 9 or 10 chars long')
+#     length_description = 'semi short'
+# else:
+#     print(f'Name "{name}" is a short name')
+#     length_description = 'short'
+
+a = 330
+b = 200
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is smaller than b")
