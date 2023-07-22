1bf2192a8e6de4fa603f1af7ebd54ecc0a4ac961
Ola Senatov <ola@senatov.pro>
2023-07-22 17:38:23 +0300

    Daily challenge Day_5

--- WEEK_2/DAY_5/DailyChallenge/DailyChallenge.py
+++ WEEK_2/DAY_5/DailyChallenge/DailyChallenge.py
@@ -1,1 +1,31 @@
-#Challenge_1
+# Challenge_1
+
+# def main():
+#   user_input = input("Enter words separated by comma: ")
+#   words = [word.strip() for word in user_input.split(",")]
+#     #print(words)
+#   separate_words = sorted(words)
+#   output = ",".join(separate_words)
+#   print(f"Sorted words are: {output}")
+  
+# main ()
+
+
+#challenge_2
+
+def longest_word(sentence):
+    words = sentence.split()
+    longest = ""
+    
+    for word in words:
+        clean_word = "".join(c for c in word if c.isalnum())
+        if len(clean_word) > len(longest):
+            longest = clean_word
+    return longest
+
+def main():
+    input_sentence = input("Enter a sentence: ")
+    result = longest_word(input_sentence)
+    print(f"The Longest Word is: {result}")
+    
+main()
