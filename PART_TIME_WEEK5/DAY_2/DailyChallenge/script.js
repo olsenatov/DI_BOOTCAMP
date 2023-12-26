e9a1dcba01b0a41f1033533541c0358fb1987c0c
Ola Senatov <ola@senatov.pro>
2023-12-26 23:35:58 +0200

    PartTime_DailyChallenge_W5_D2

--- PART_TIME_WEEK5/DAY_2/DailyChallenge/script.js
+++ PART_TIME_WEEK5/DAY_2/DailyChallenge/script.js
@@ -11,3 +11,40 @@
 // Write a story that uses each of the values.
 // Make sure you check the console for errors when playing the game.
 // Bonus: Add a “shuffle” button to the HTML file, when clicked the button should change the story currently displayed (but keep the values entered by the user). The user could click the button at least three times and get a new story. Display the stories randomly.
+
+const form = document.getElementById('libform');
+const noun = document.getElementById('noun');
+const adjective = document.getElementById('adjective');
+const person = document.getElementById('person');
+const verb = document.getElementById('verb');
+const place = document.getElementById('place');
+const story = document.getElementById('story');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    getAllInputsValues()
+})
+
+function getAllInputsValues() {
+    if(!validateValueInput(noun.value) ||
+    !validateValueInput(adjective.value) ||
+    !validateValueInput(person.value) ||
+    !validateValueInput(verb.value) ||
+    !validateValueInput(place.value)) {
+        return;
+    };
+    const mystory = `Far far away in a ${adjective.value} ${place.value}, there lived
+    a ${person.value} and he had a magic ${noun.value}. Once he ${verb.value} far away from ${place.value}, 
+    and he was rather scared to see himself ${adjective.value}`;
+
+    story.innerText = mystory;
+}
+
+function validateValueInput(value) {
+    if (value.trim().length === 0) {
+        alert("Please enter a value");
+        return false;
+    }
+    return true;
+}
+
