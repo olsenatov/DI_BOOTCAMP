e510419533328ba52c661d7933a658d2e40bed76
Ola Senatov <ola@senatov.pro>
2024-01-07 11:55:22 +0200

    PART_TIME_ExercisesXP_DailyChallenge_day5

---
+++ PART_TIME_WEEK6/DAY_5/DailyChallenge/DailyChallenge.sql
@@ -0,0 +1,23 @@
+-- create table actors (
+-- id serial primary key,
+-- first_name varchar,
+-- last_name varchar,
+-- age date,
+-- number_oscars int )
+-- ;
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES 
+-- ('Matt', 'Damon', '1970-08-10', 5),
+-- ('George', 'Clooney', '1961-06-05', 2),
+-- ('Angelina', 'Jolie', '1975-06-04', 1),
+-- ('Jennifer', 'Aniston', '1969-02-11', 0)
+
+-- SELECT COUNT(*) from actors
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars) 
+-- VALUES ('Jim', 'Carrey', '1962-01-17', Null)
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars) 
+-- VALUES ('Julia', 'Roberts', '1967-10-28')  
+-- -- this returns Error INSERT has more target columns than expressions
